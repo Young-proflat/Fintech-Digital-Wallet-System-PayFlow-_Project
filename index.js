55d29ae295212d3602d08d0c69f33d1bccdcfc3c
Ado Abdullateef <105428038+Young-proflat@users.noreply.github.com>
2025-05-16 15:35:45 +0000

    Updated the repo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express()
-const DATABASE_URL = "mongodb+srv://:@cluster0.ucmgyxj.mongodb.net/?=true&w=majority&appName=Cluster0";
 
 const PORT = process.env.PORT || 3000
-mongoose.connect(DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL)
 .then( ()=>{
     console.log("Database is running")
     app.listen(PORT, ()=> {
